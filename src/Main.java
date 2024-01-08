8329de52a960d58084cb443afff5c9ce1996d398
Claudio V. Carta <claudio.vittorio.carta@gmail.com>
2024-01-08 10:10:20 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Coordinate punto1 = new Coordinate(1, 2);
+        Coordinate punto2 = new Coordinate(3, 3);
+
+        System.out.println(punto1);
+        System.out.println(punto2);
+    }
+}
